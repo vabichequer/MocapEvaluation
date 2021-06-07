119918c74ac9a1e9f377096c49048ef2add66a8c
Vicenzo <v.a.s.i@outlook.com>
2021-06-07 15:01:52 +0200

    Correction in the calculation of angles
    I was calculating orientation instead of angular velocity

--- main.py
+++ main.py
@@ -3,8 +3,16 @@ import matplotlib.pyplot as plt
 import math 
 import csv
 
+ANNOTATE = False
+
 f = 120
-dt = 1/120
+dt = 1/f
+
+print('*' * 25)
+print("Program setup:")
+print("Frequency (Hz): ", f)
+print("dt: ", dt)
+print('*' * 25)
 
 # Print iterations progress
 def printProgressBar (iteration, total, prefix = '', suffix = '', decimals = 1, length = 100, fill = '█', printEnd = "\r"):
@@ -33,8 +41,8 @@ y = []
 
 with open('root_mocap.csv', newline='') as csvfile:
     read_csv = csv.reader(csvfile, delimiter=',', quotechar='|')
-    next(read_csv, None)
-    for row in read_csv:
+    next(read_csv, None) # skipping the first line
+    for i, row in enumerate(read_csv):
         x.append(float(row[1]))
         y.append(float(row[3]))
 
@@ -48,11 +56,42 @@ for i in range(1, len(x)):
     dx = x[i] - x[i - 1]
     dy = y[i] - y[i - 1]
 
-    speed.append(np.linalg.norm(np.array([dx, dy])) / 2*dt)
+    t0 = math.degrees(math.atan2(y[i - 1], x[i - 1]))
+    t1 = math.degrees(math.atan2(y[i], x[i]))
 
-    theta.append(math.degrees(math.atan2(dx, dy)))
+    dtheta = t1 - t0
 
-plt.ylabel("Speed")
-plt.xlabel("Turning angle")
+    if (abs(dtheta) >= 180):
+        print('-' * 25)
+        print("dtheta: ", dtheta)
+        dec, int = math.modf(dtheta / 180)
+        if (dec == 0):
+            dtheta = 0
+        else:
+            degree = (abs(dec) - 1) * 180
+            actual = -math.copysign(degree, dtheta)
+            dtheta = actual
+            print("degree: ", degree)
+            print("dec: ", dec)
+            print("actual: ", actual)
+        print('-' * 25)
+
+    speed.append(np.linalg.norm(np.array([dx, dy])) / dt)
+
+    theta.append(dtheta / dt)
+
+fig, ax = plt.subplots()
+plt.ylabel("Speed (m/s)")
+plt.xlabel("Turning speed (degrees/s)")
 plt.scatter(theta, speed, marker='+')
+
+
+if (ANNOTATE):
+    txt = 0.1
+    for i in range(0, len(speed)):
+        if (i % 10 == 0):
+            ax.annotate("%.2f" % txt, (theta[i], speed[i]))
+        txt += 0.1
+
+
 plt.show()
