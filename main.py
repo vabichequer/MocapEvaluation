fd3dc2546ad8fe145c1e4262dd6af0dd61f7b335
Vicenzo <v.a.s.i@outlook.com>
2021-10-06 15:56:04 +0200

    [New] Support for saving graphs and also input are now plotted together

--- main.py
+++ main.py
@@ -1,45 +1,21 @@
-from matplotlib.text import Annotation
 import numpy as np
 import matplotlib.pyplot as plt
 import math 
 import csv
 import sys
 from pathlib import Path
-from distutils.util import strtobool
-import mpl_scatter_density # adds projection='scatter_density'
-from matplotlib.colors import LinearSegmentedColormap
 
 ### ARGUMENTS ###
 # <string> CSV file name
 # <int> sampling rate
 
-# "Viridis-like" colormap with white background
-white_viridis = LinearSegmentedColormap.from_list('white_viridis', [
-    (0, '#ffffff'),
-    (1e-20, '#440053'),
-    (0.2, '#404388'),
-    (0.4, '#2a788e'),
-    (0.6, '#21a784'),
-    (0.8, '#78d151'),
-    (1, '#fde624'),
-], N=256)
-
-def using_mpl_scatter_density(fig, x, y):
-    ax = fig.add_subplot(1, 1, 1, projection='scatter_density')
-    density = ax.scatter_density(x, y, cmap=white_viridis)
-
-    fig.colorbar(density, label='Number of points per pixel')
-    ax.set_ylabel("Linear speed (m/s)")
-    ax.set_xlabel("Turning speed (degrees/s)")
-    ax.set_title("Speed coverage map")
-
-def scatter(fig, x, y, alpha, color):    
+def scatter(fig, x, y, alpha, color, radius):    
     ax = fig.add_subplot(1, 1, 1)
     ax.scatter(x, y, alpha=alpha, c=color)
 
     ax.set_ylabel("Linear speed (m/s)")
     ax.set_xlabel("Turning speed (degrees/s)")
-    ax.set_title("Speed coverage map")
+    ax.set_title("Speed coverage map, radius: " + str(radius))
 
 def magnitude(vector): 
     return math.sqrt(sum(pow(element, 2) for element in vector))
@@ -80,7 +56,7 @@ for file_idx, file in enumerate(csv_file_name):
     ry = []
     t = []
 
-    with open(file, newline='') as csvfile:
+    with open(FOLDER_FILES + '/' + file, newline='') as csvfile:
         csv_reader = csv.reader(csvfile, delimiter=',', quotechar='|')    
         actual_sample = 0
         for i, row in enumerate(csv_reader):   
@@ -173,33 +149,38 @@ for file_idx, file in enumerate(csv_file_name):
     speed_arrays.append(speed)
     theta_arrays.append(theta)
 
-
-fig_mpl = plt.figure()
-colors = ["red", "blue", "yellow"]
-alphas = [0.5, 0.25, 1]
-
 file_nbr = len(csv_file_name)
 
-for i in range(0, file_nbr):
-    speed = speed_arrays[i]
-    theta = theta_arrays[i]
-    scatter(fig_mpl, theta, speed, alphas[i], colors[i])
+for i, r in enumerate(radius):
+    fig_scm = plt.figure()
 
+    speed = speed_arrays[0]
+    theta = theta_arrays[0]
+    scatter(fig_scm, theta, speed, 0.5, "red", r)
 
-    fig = plt.figure()
+    speed = speed_arrays[i + 1]
+    theta = theta_arrays[i + 1]
+    scatter(fig_scm, theta, speed, 0.25, "blue", r)
+
+    fig_ts = plt.figure()
     plt.ylabel("Turning speed (degrees/s)")
     plt.title("Turning speed (degrees/s) vs time (s)")
-    plt.plot([i for i in range(0, len(theta))], theta)
+    plt.plot([j for j in range(0, len(theta))], theta)
+    plt.plot([l for l in range(0, len(theta))], [theta.mean() for m in range(0, len(speed))])
 
 
-    fig = plt.figure()
+    fig_ls = plt.figure()
     plt.ylabel("Linear speed (m/s)")
     plt.title("Linear speed (m/s) vs time (s)")
-    plt.plot([i for i in range(0, len(speed))], speed)
-    plt.plot([i for i in range(0, len(speed))], [speed.mean() for i in range(0, len(speed))])
+    plt.plot([k for k in range(0, len(speed))], speed)
+    plt.plot([l for l in range(0, len(speed))], [speed.mean() for m in range(0, len(speed))])
 
-for i, r in enumerate(radius):
     info = read_csv(r, sufixes=["info"])
-    scatter(fig_mpl, info["Angle speed"], info["Desired linear speed"], alphas[file_nbr + i], colors[file_nbr + i])
+    scatter(fig_scm, info["Angle speed"], info["Desired linear speed"], 1, "yellow", r)
 
-plt.show()
+    fig_scm.set_size_inches((16, 9), forward=False)
+    fig_scm.savefig(str(r) + "_scm.eps", )
+    fig_ts.savefig(str(r) + "_ts.eps")
+    fig_ls.savefig(str(r) + "_ls.eps")
+
+    plt.show()
